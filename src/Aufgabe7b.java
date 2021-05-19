3f225b2968794262f2ba12562557e934ef8e8380
laserrusher <buihaianh@outlook.de>
2021-05-19 14:58:44 +0200

    aufgabe 13 in bearbeitung

--- src/Aufgabe7b.java
+++ src/Aufgabe7b.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Aufgabe7b {
+public class Aufgabe7b {
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder("");
+        for (int i = 0; i < args.length; i++) {
+            s.insert(0, args[0]);
+            System.out.println(s.toString());
+        }
+        s.reverse();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '-') {
+                s.setCharAt(i, '_');
+            }
+        }
+        System.out.println(s.toString());
+
+    }
 }
